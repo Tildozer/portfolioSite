95eec1d114992edff0cb70fbd7a8f949d06e7ad0
Tildozer <actninswitch@gmail.com>
2023-03-22 16:47:57 -0600

    got my app up and running with tailwind and ready to work on

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+// import './globals.css';
+
+export default async function About() {
+  return (
+    <main className='bg-orange-500 min-h-[100vh]'>
+      <div className='text-gray-800'>
+        about
+      </div>
+    </main>
+  )
+}
